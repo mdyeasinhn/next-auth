3c4c156e3c93446649e781f6a8a01fdde13c6142
mdyeasinhn <mdyeasinhn@gmail.com>
2025-06-30 14:27:04 +0600

    Protect a route from unauthorized users using middleware

--- nextjs-auth-client/src/utils/authOptions.ts
+++ nextjs-auth-client/src/utils/authOptions.ts
@@ -14,6 +14,9 @@ export const authOptions :NextAuthOptions = {
     clientSecret: process.env.GOOGLE_SECRET as string
   }),
   ],
+  pages: {
+    signIn : "/login"
+  },
   secret :process.env.NEXTAUTH_SECRET
 }
 
